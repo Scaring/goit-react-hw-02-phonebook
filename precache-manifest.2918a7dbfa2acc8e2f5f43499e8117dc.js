d3ad3f3f3e595926e94cce1a2821087e8d47631f
Dmytro Kuschevyy <scaring22@gmail.com>
2020-04-24 14:00:50 +0300

    Updates

--- precache-manifest.2918a7dbfa2acc8e2f5f43499e8117dc.js
+++ precache-manifest.2918a7dbfa2acc8e2f5f43499e8117dc.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c9d47a198b3b9a8d7aa98820091dc0c",
+    "revision": "752784a527f4211f22f95ccbce6ba21d",
     "url": "/goit-react-hw-02-phonebook/index.html"
   },
   {
-    "revision": "ede75bda9eb4765b3ec9",
+    "revision": "07844643ba7afce8ce0b",
     "url": "/goit-react-hw-02-phonebook/static/css/main.ceb1b981.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02-phonebook/static/js/2.5662b1a4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ede75bda9eb4765b3ec9",
-    "url": "/goit-react-hw-02-phonebook/static/js/main.c47ae52e.chunk.js"
+    "revision": "07844643ba7afce8ce0b",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.9c4bbd25.chunk.js"
   },
   {
     "revision": "bfccad9aa0892efebfa9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-phonebook/precache-manifest.a1fca26a6b277891672c9bec64ba1184.js"
+  "/goit-react-hw-02-phonebook/precache-manifest.2918a7dbfa2acc8e2f5f43499e8117dc.js"
 );
 
 self.addEventListener('message', (event) => {
